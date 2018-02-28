0057d4fa687809691d13f1eacc1a73f4a16345d2
hanxiao <hanxiao@maxtropy.com>
2018-02-28 20:30:09 +0800

    dva-cli初始化

---
+++ dva-quickstart/src/components/ProductList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Table, Popconfirm, Button} from 'antd';
+
+const ProductList = ({onDelete, products}) => {
+  const columns = [{
+    title: 'name',
+  }]
+}
